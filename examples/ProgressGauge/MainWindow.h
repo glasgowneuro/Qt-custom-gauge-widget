93a1320789ea28f79568e3b4c17b8e9a53e3cf94
Fpepe943 <fpepe943@gmail.com>
2021-07-25 11:58:32 +0200

    added custom ProgressBar

---
+++ examples/ProgressGauge/MainWindow.h
@@ -0,0 +1,39 @@
+#ifndef MAINWINDOW_H
+#define MAINWINDOW_H
+
+#include <QMainWindow>
+#include "RoundGaugeGraphicsObject.h"
+
+class QGraphicsScene;
+class QPushButton;
+
+namespace Ui {
+class MainWindow;
+}
+
+class RoundGaugeGraphicsObject;
+class NeedleMeterGraphicsObject;
+
+class MainWindow : public QMainWindow
+{
+    Q_OBJECT
+
+public:
+    explicit MainWindow(QWidget *parent = nullptr);
+    ~MainWindow();
+
+private slots:
+    void on_pushButton_clicked();
+
+private:
+    Ui::MainWindow *ui;
+
+    QGraphicsScene* mScene;
+
+    RoundGaugeGraphicsObject* mRoundGauge;
+    NeedleMeterGraphicsObject* mNeedleMeter;
+
+    void setAGaugeColor(QPushButton* button);
+};
+
+#endif // MAINWINDOW_H
